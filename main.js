feb6a255cb90780d0dd8642d3565dec299ab6443
Mateusz Siuda <131684931+Mateusz627@users.noreply.github.com>
2023-06-18 16:00:53 +0200

    Adding variables

---
+++ main.js
@@ -0,0 +1,19 @@
+const currentNumber = document.querySelector('.currentNumber')
+
+const previousNumber = document.querySelector('.previousNumber p')
+
+const mathSign = document.querySelector('.mathSign')
+
+const numbersButtons = document.querySelector('.number')
+
+const operatorsButtons = document.querySelectorAll('.operator')
+
+const equalsButton = document.querySelector('.equals')
+
+const clearButton = document.querySelector('.clear')
+
+const calculatorHistory = document.querySelector('.history')
+
+const historyBtn = document.querySelector('.history-btn')
+
+let result = ''
